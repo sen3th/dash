570f987f3d04aa9289c75447fea683e3f16cec88
Seneth <hi@seneth.me>
2025-11-14 20:42:58 -0500

    added animated background. added copyright footer.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//date display script
+
 const dateDisplayElement = document.getElementById("currentDate");
 
 const today = new Date();
@@ -7,4 +9,3 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 const formattedDate = today.toLocaleDateString(undefined, options);
 
 dateDisplayElement.innerHTML = formattedDate;
-
