c74b882a1c55854868d83193f3f56f6c4c0133c9
Seneth <hi@seneth.me>
2025-11-14 20:14:03 -0500

    initial commit, added current date box, added title

---
+++ script.js
@@ -0,0 +1,10 @@
+const dateDisplayElement = document.getElementById("currentDate");
+
+const today = new Date();
+
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+const formattedDate = today.toLocaleDateString(undefined, options);
+
+dateDisplayElement.innerHTML = formattedDate;
+
